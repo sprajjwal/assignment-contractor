b26a85e14083e23abaaf97180f1641ec25366fd5
sprajjwal <sprajjwal@ucdavis.edu>
2019-10-06 17:54:20 -0700

    [ADD]multiple routes and forms added

--- app.py
+++ app.py
@@ -4,18 +4,47 @@ from bson.objectid import ObjectId
 import os
 from datetime import datetime
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=f'{host}?retryWrites=false')
-db = client.get_default_database()
-playlists = db.playlists
+# host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/contractor')
+# client = MongoClient(host=f'{host}?retryWrites=false')
+# db = client.get_default_database()
+client = MongoClient()
+db = client.contractor
+items = db.items
 
-db = client.get_default_database()
-playlists = db.playlists
-comments = db.comments
 
 app = Flask(__name__)
 
 @app.route('/')
-def playlists_index():
+def store_index():
     """Show all playlists."""
-    return render_template('store_home.html', playlists=playlists.find())
+    return render_template('store_index.html', items=items.find())
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    """Create and add a new item to the database"""
+    item = {
+        'name': request.form.get('name'),
+        'price': request.form.get('price'),
+        'in_stock': request.form.get('in'),
+        'images': request.form.get('images').split()
+    }
+    item_id = items.insert_one(item).inserted_id
+    return redirect(url_for('item', item_id=item_id))
+
+@app.route('/items/new')
+def items_new():
+    return render_template('items_new.html', item={}, title='New Item')
+
+# @app.route('/items')
+# def store_items():
+#     return render_template('items.html', items=items.find())
+
+@app.route('/items/<item_id>')
+def item(item_id):
+    item = items.find_one({'_id': ObjectId(item_id)})
+    return render_template('item.html', item=item)
+
+if __name__ == '__main__':
+    # playlists.delete_many({})
+    # comments.delete_many({})
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
